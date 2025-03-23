45be2b20479e2ed94ba5540dfa4e720773f044cf
Matías Games <gabrielgamesmorgani@gmail.com>
2025-03-23 19:25:12 -0300

    Subiendo columna sprints maquetacion

---
+++ todo-advanced/src/components/ui/CardSprints/CardSprints.tsx
@@ -0,0 +1,11 @@
+import styles from './CardSprints.module.css'
+
+export const CardSprints = () => {
+  return (
+    <div className={styles.containerCardSprints}>
+        <p>sprint 1</p>
+        <p>Inicio: 02.05.25</p>
+        <p>Cierre: 10.06.25</p>
+    </div>
+  )
+}
--- todo-advanced/src/components/ui/ColumnSprints/ColumnSprints.tsx
+++ todo-advanced/src/components/ui/ColumnSprints/ColumnSprints.tsx
@@ -1,10 +1,15 @@
+import { CardSprints } from '../CardSprints/CardSprints'
 import styles from './ColumnSprints.module.css'
 
 
 export const ColumnSprints = () => {
   return (
     <div className={styles.containerColumnSprints}>
-        
+        <button className={styles.buttonVolverColumnSprints}>
+            Volver
+        </button>
+        <h2>sprints</h2>
+        < CardSprints />
     </div>
   )
 }
