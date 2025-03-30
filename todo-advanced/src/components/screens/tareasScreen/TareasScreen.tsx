1b0756c915314e34d0ff148a963fa226b2a3af18
Jorge Garcia Ojeda <jor4300@gmail.com>
2025-03-30 13:45:08 -0300

    se crearon las funciones crud del backlog

--- todo-advanced/src/components/screens/tareasScreen/TareasScreen.tsx
+++ todo-advanced/src/components/screens/tareasScreen/TareasScreen.tsx
@@ -1,17 +1,16 @@
-import { ColumnSprints } from "../../ui/ColumnSprints/ColumnSprints"
-import { Header } from "../../ui/Header/Header"
-import { ProgressTasks } from "../../ui/ProgressTasks/ProgressTasks"
-import styles from './TareasScreen.module.css'
-
+import { ColumnSprints } from "../../ui/ColumnSprints/ColumnSprints";
+import { Header } from "../../ui/Header/Header";
+import { ProgressTasks } from "../../ui/ProgressTasks/ProgressTasks";
+import styles from "./TareasScreen.module.css";
 
 export const TareasScreen = () => {
   return (
     <div>
-        <Header />
-        <div className={styles.containerMain}>
-        <ColumnSprints texto="Backlog"/>
+      <Header />
+      <div className={styles.containerMain}>
+        <ColumnSprints texto="Backlog" />
         <ProgressTasks />
       </div>
     </div>
-  )
-}
+  );
+};
--- todo-advanced/src/http/backLog.ts
+++ todo-advanced/src/http/backLog.ts
@@ -3,12 +3,42 @@ import { ITarea } from "../types/ITarea";
 import { IBacklog } from "../types/IBacklog";
 import { API_URL } from "../utils/constantes";
 
-export const putBacklog = async(tareas: ITarea[]) => {
-    try{
-        const response = await axios.put<IBacklog[]>(API_URL, {tareas: tareas})
+//Crear Tareaa
+export const createTask = async (task: ITarea) => {
+  try {
+    const response = await axios.post<IBacklog[]>(API_URL, { task: task });
+    return response.data;
+  } catch (error) {
+    console.log("Error al crear la tarea", error);
+  }
+};
+//Eliminar tarea
+export const deleteTask = async (idTask: string) => {
+  try {
+    const response = await axios.delete(`/tareas/${idTask}`);
+    return response.data;
+  } catch (error) {
+    console.log("No se pudo eliminar la tarea", error);
+  }
+};
+//Mostrar tareas
 
-        return response.data
-    }catch(error){
-        console.log("Algo salio mal al editar las tareas del backlog, " + error)
-    }
-}
+export const getAllTareas = async () => {
+  try {
+    const response = await axios.get<IBacklog[]>(API_URL);
+    return response.data;
+  } catch (error) {
+    console.log("Error al traer las tareas ", error);
+  }
+};
+
+//Editar tarrea
+export const editTask = async (tareas: ITarea[]) => {
+  try {
+    const response = await axios.put<IBacklog[]>(API_URL, { tareas: tareas });
+
+    return response.data;
+  } catch (error) {
+    console.log("Algo salio mal al editar las tareas del backlog, ", error);
+  }
+};
